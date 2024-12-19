14c5238b846031e351c106143ecd259f4ed2531c
venkatagowthamjalam <venkatgowtham98@gmail.com>
2024-12-20 02:53:00 +0530

    Palindrome

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,3 +1,18 @@
+// https://leetcode.com/problems/palindrome-number/description/
 public class Palindrome {
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length();
 
+        while(left < right)
+        {
+            if(s.charAt(left) != s.charAt(right))
+            {
+                return false;
+            }
+            left++;
+            right++;
+        }
+        return true;
+    }
 }
