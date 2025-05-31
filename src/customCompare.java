fe82a095ca296ab4430b8f184062944fafb42efe
venkatagowthamjalam <venkatgowtham98@gmail.com>
2025-05-31 10:15:19 -0700

    reverse linked list

--- src/customCompare.java
+++ src/customCompare.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class customCompare {
+import java.util.*;
+public class customCompare {
+    public static void main(String[] args) {
+
+
+       Comparator com = new Comparator() {
+           @Override
+           public int compare(Object o1, Object o2) {
+               String s1 = (String) o1;
+               String s2 = (String) o2;
+               if (s1.length() > s2.length())
+                   return 1;
+               else
+                   return  -1;
+           }
+       };
+
+        List<String> l = new ArrayList<>();
+
+
+        l.add("Gowtham");
+        l.add("Sai");
+        l.add("Manoj");
+        l.add("Jalam Uma");
+        l.add("Sourya");
+
+        Collections.sort(l, com);
+
+        System.out.println(l);
+
+
+
+    }
 }
