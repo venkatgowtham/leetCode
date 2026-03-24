889c599c716893a7d35ed2cdd45e66ae59275fb2
venkatgowtham <53149835+venkatgowtham@users.noreply.github.com>
2026-03-23 23:22:45 -0700

    Time: 27 ms (33.11%) | Memory: 12.9 MB (60.03%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        left = 0
+        right = len(s)-1
+        s= lower(s)
+        print(s)
+        while left <= right:
+            while left < right and not(s[left] >= 'a' and s[left]<='z') and not(s[left] >= '0' and s[left]<='9'):
+                left+=1
+            while left < right and not(s[right] >= 'a' and s[right]<='z') and not(s[right] >= '0' and s[right]<='9'):
+                right -=1
+            if s[left] != s[right]:
+                print(left, s[left], "right", right, s[right])
+                return False
+            left +=1 
+            right -= 1
+        
+        return True
+
+
+        
